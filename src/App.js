5ac34ad56418fa313270f2d02dc9b749ec984754
Bogdan Negura <id.bogdann@yahoo.com>
2019-09-22 22:02:02 +0100

    create the componet Navbar and apply some props methods

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component {
 
   render() {
-      return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-    );
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+      );
   }
 }
 

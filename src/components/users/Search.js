25329f7155e88ae9b5a9857fa931004be513baa6
Bogdan Negura <id.bogdann@yahoo.com>
2019-09-24 11:36:45 +0100

    add event on search component and passing props up and search

---
+++ src/components/users/Search.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Search extends Component {
+
+    state = {
+        text: ''
+    }
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired
+    }
+
+    onSubmit = e => {
+        e.preventDefault()
+        this.props.searchUsers(this.state.text)
+        this.setState({ text: '' })
+    }
+
+    OnChange = e => this.setState({ text: e.target.value })
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.onSubmit} className="form">
+                    <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.OnChange}/>
+                    <input type="submit" value="Search" className="btn btn-dark btn-block" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Search

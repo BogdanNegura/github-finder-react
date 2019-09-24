cedcd83b2157b0ee0126fddd2aa42c054f6bf3ce
Bogdan Negura <id.bogdann@yahoo.com>
2019-09-24 12:19:21 +0100

    a little bit of destructuring

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -22,13 +22,16 @@ export class Search extends Component {
     OnChange = e => this.setState({ text: e.target.value })
 
     render() {
+
+        const { showClear, clearUsers } = this.props
+
         return (
             <div>
                 <form onSubmit={this.onSubmit} className="form">
                     <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.OnChange}/>
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
-                {this.props.showClear && (<button className="btn btn-light btn-block" onClick={this.props.clearUsers} >Clear</button> )}
+                {showClear && (<button className="btn btn-light btn-block" onClick={clearUsers} >Clear</button> )}
                 
             </div>
         )

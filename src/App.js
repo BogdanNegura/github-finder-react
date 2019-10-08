a3a67a1ae0854cfbf3443ef9d5abe99a099ddb2c
Bogdan Negura <id.bogdann@yahoo.com>
2019-10-08 23:19:06 +0100

    moving User State to Context

--- src/App.js
+++ src/App.js
@@ -14,22 +14,10 @@ import './App.css';
 
 const App = () => {
 
-  const [users, setUsers ] = useState([])
-  const [user, setUser ] = useState({})
   const [repos, setRepos ] = useState([])
   const [loading, setLoading] = useState(false)
   const [alert, setAlert ] = useState(null)
 
-  // get Single GitHub user
-  const getUser = async username => {
-    setLoading(true);
-    
-    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-    client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    
-    setUser(res.data)
-    setLoading(false)
-  }
 
   // Get Users repos
   const getUserRepos = async username => {
@@ -43,13 +31,6 @@ const App = () => {
   }
 
   
-
-  // clear users from state
-  const clearUsers = () => {
-    setUsers([])
-    setLoading(false)
-  }
-
   // Set Alert
   const showAlert = (msg, type) => {
     setAlert({ msg, type })
@@ -69,10 +50,8 @@ const App = () => {
               <Route exact path='/' render={props => (
                 <Fragment>
                   <Search 
-                    clearUsers={clearUsers} 
-                    showClear={users.length > 0 ? true : false} 
                     setAlert={showAlert} />
-                  <Users loading={loading} users={users} />
+                  <Users />
                 </Fragment>
               )} />
               <Route exact path='/about' component={About} />
@@ -80,11 +59,8 @@ const App = () => {
                     path='/user/:login' 
                     render={props => ( 
                       <User {...props} 
-                            getUser={getUser} 
                             getUserRepos={getUserRepos} 
-                            user={user}
                             repos={repos}
-                            loading={loading} 
                       /> 
                       )} 
                 />
